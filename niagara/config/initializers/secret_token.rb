7cb5ea52cdd5d7e8ad9f20faccadaffbcd842b31
JustinJiaWen <justin.wen.jia@gmail.com>
2012-12-05 15:55:40 +0800

    first update

---
+++ niagara/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Niagara::Application.config.secret_token = '8b692c158c3b77e3e42ca11b682db01944066e5ace1a41685e89118acca7970e4fa46a60ccc450424e1fd9e75584d75782cd471a98c55c62eb3d05d2703d07f0'
